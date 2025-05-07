1b6b585b9e70926b497eb5b8621cd91e25084520
Siddhartha Gurram <siddharthags29@gmail.com>
2025-05-06 19:38:27 -0500

    completed navbar and home page layout

---
+++ frontend/food_blog_app/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+ import React from 'react'
+
+ export default function Navbar() {
+    return (
+        <>
+            <header>
+                <h2>Food Blog</h2>
+                <ul>
+                    <li>My Recipe</li>
+                    <li>Favourites</li>
+                    <li>Login</li>
+
+                </ul>
+                
+
+            </header>
+        </>
+    )
+ }
--- frontend/food_blog_app/src/pages/Home.jsx
+++ frontend/food_blog_app/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import foodRecipe from '../assets/foodRecipe.jpg'
+import Navbar from '../components/Navbar'; // adjust the path as needed
+import Footer from '../components/Footer';
+
 export default function Home() {
   return (
     <>
+         <Navbar/>
          <section className='home'>
              <div className='left'>
                  <h1>Food Recipe</h1>
@@ -10,12 +14,13 @@ export default function Home() {
                  <button>share your recipe here</button>
              </div>
              <div className='right'>
-                 <img src={foodRecipe} width="490px" height="370px"></img>
+                 <img src={foodRecipe} width="490px" height="370px"/>
             </div>
         </section>
         <div className='bg'>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#d4f6e8" fillOpacity="1" d="M0,224L26.7,224C53.3,224,107,224,160,186.7C213.3,149,267,75,320,80C373.3,85,427,171,480,213.3C533.3,256,587,256,640,261.3C693.3,267,747,277,800,245.3C853.3,213,907,139,960,122.7C1013.3,107,1067,149,1120,149.3C1173.3,149,1227,107,1280,74.7C1333.3,43,1387,21,1413,10.7L1440,0L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"></path></svg>
         </div>
+        <Footer/>
     </>
   )
 }
